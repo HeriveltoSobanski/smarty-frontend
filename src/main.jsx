367d0c6b4c14516d3bf47a5ccd7112e646f9de7c
Herivelto Sobanski <ens-heriveltosobanski@ugv.edu.br>
2025-10-06 22:06:03 -0300

    pagina principal agora é login

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+
+import Login from "./pages/Login.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
